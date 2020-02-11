2de919622e2c24a8b7b1075c04507c00968b7e11
Rohan Rawal <rohanrawal-adapty>
2020-02-11 19:01:12 +0530

    Final Changes

--- registration/script.js
+++ registration/script.js
@@ -6,38 +6,60 @@ function validate(){
     let firstName = document.StudentRegistration.firstName.value;
     let lastName = document.StudentRegistration.lastName.value;
    
+   
     if(emailId == ""){
         document.getElementById('emailIdError').innerHTML = "** EmailId cannot be empty";
-        document.getElementById('emailId').focus()
+        document.getElementById('emailId').style.border  ='2px solid red';
         return false;
+    }else{
+        document.getElementById('emailIdError').innerHTML = "";
+        document.getElementById('emailId').style.border  ='';
     }
+
     if(password == ""){
         document.getElementById('passwordError').innerHTML = "** Password cannot be empty";
-        document.getElementById('passwordError').focus()
+        document.getElementById('password').style.border  ='2px solid red';
         return false;
+    }else{
+        document.getElementById('passwordError').innerHTML = "";
+        document.getElementById('password').style.border  ='';
     }
+
     if(confirmPassword == ""){
         document.getElementById('confirmPasswordError').innerHTML = "** ConfirmPassword cannot be empty";
-        document.getElementById('confirmPasswordError').focus()
+        document.getElementById('confirmPassword').style.border  ='2px solid red';
         return false;
+    }else{
+        document.getElementById('confirmPasswordError').innerHTML = "";
+        document.getElementById('confirmPassword').style.border  ='';
     }
+
     if(firstName == ""){
         document.getElementById('firstNameError').innerHTML = "** FirstName cannot be empty";
-        document.getElementById('firstNameError').focus()
+        document.getElementById('firstName').style.border  ='2px solid red';
         return false;
+    }else{
+        document.getElementById('firstNameError').innerHTML = "";
+        document.getElementById('firstName').style.border  ='';
     }
+
     if(lastName == ""){
         document.getElementById('lastNameError').innerHTML = "** LastName cannot be empty";
-        document.getElementById('emailId').focus()
+        document.getElementById('lastName').style.border  ='2px solid red';
         return false;
+    }else{
+        document.getElementById('lastNameError').innerHTML = "";
+        document.getElementById('lastName').style.border  ='';
     }
 
     if(password != confirmPassword){
         document.getElementById('confirmPasswordError').innerHTML = "** Password doesnot match";
-        document.getElementById('lastNameError').focus()
+        document.getElementById('confirmPassword').style.border  ='2px solid red';       
         return false;
+    }else{
+        document.getElementById('confirmPasswordError').innerHTML = "";
+        document.getElementById('confirmPassword').style.border  ='';
     }
 
-    
 }
 
